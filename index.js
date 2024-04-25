761589994e70be8f13613e25196c33c67dc95b13
Mike Hodnick <mike@kindohm.com>
2024-04-24 20:43:52 -0500

    guard against the word commit in commit messages

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const { exec } = require("child_process");
 const fs = require("fs");
 const { add } = require("date-fns");
 
+const CHUNK_DAY_SIZE = 10;
+
+const getCommitsFromOutput = (output) => {
+
+  const splits = output.split("\ncommit ")
+
+  const filtered = splits.filter(c => c.trim().length > 0);
+  return filtered;
+
+}
+
 const getCommits = ({ previousCommits, startDate, endDate }) => {
   return new Promise((res, rej) => {
     const startDateString = startDate.toISOString();
     const endDateString = endDate.toISOString();
 
+    console.log(startDateString);
+
     exec(
-      `cd ~/code/thrivent-web && git log --numstat --pretty --after=${startDateString} --before=${endDateString}`,
+      `cd ~/code/react && git log --numstat --pretty --after=${startDateString} --before=${endDateString}`,
       { maxBuffer: 1024 * 10000 },
       async (error, stdout, stderr) => {
         if (error) {
@@ -21,38 +34,47 @@ const getCommits = ({ previousCommits, startDate, endDate }) => {
           return;
         }
 
-        const raw = stdout.split("commit ").filter((c) => c.trim().length > 0);
+      
+
+        const raw = getCommitsFromOutput(stdout); // stdout.split("commit ").filter((c) => c.trim().length > 0);
         if (raw.length === 0) {
           res(previousCommits);
         }
 
         const formatted = raw.map((commit) => {
-          const lines = commit.split("\n");
-          const sha = lines[0];
-          const author = lines[1].substring(8);
-          const dateString = lines[2].substring(8);
-          const date = new Date(dateString);
+          try {
+            const lines = commit.split("\n");
 
-          const spaceIndex = lines.findIndex((l, i) => i > 4 && l === "");
+            const sha = lines[0];
+            const author = lines[1].substring(8);
+            const dateString = lines[2].substring(8);
+            const date = new Date(dateString);
 
-          const notesLines = lines
-            .filter((l, i) => i >= 4 && i < spaceIndex)
-            .map((n) => n.substring(4));
-          const fileLines = lines
-            .filter((l, i) => i > spaceIndex)
-            .filter((l) => l.trim().length > 0);
+            const spaceIndex = lines.findIndex((l, i) => i > 4 && l === "");
 
-          const notes = notesLines.join("\n");
-          const files = fileLines.map((fl) => {
-            const [adds, rems, path] = fl.split("\t");
-            return {
-              additions: parseFloat(adds),
-              removals: parseFloat(rems),
-              path,
-            };
-          });
+            const notesLines = lines
+              .filter((l, i) => i >= 4 && i < spaceIndex)
+              .map((n) => n.substring(4));
+            const fileLines = lines
+              .filter((l, i) => i > spaceIndex)
+              .filter((l) => l.trim().length > 0);
 
-          return { sha, author, notes, files, dateString, date };
+            const notes = notesLines.join("\n");
+            const files = fileLines.map((fl) => {
+              const [adds, rems, path] = fl.split("\t");
+              return {
+                additions: parseFloat(adds),
+                removals: parseFloat(rems),
+                path,
+              };
+            });
+
+            return { sha, author, notes, files, dateString, date };
+          } catch (err) {
+            console.error("error doing stuff", err.message);
+            console.error('commit', commit)
+            throw err;
+          }
         });
 
         res(formatted);
@@ -63,7 +85,7 @@ const getCommits = ({ previousCommits, startDate, endDate }) => {
 
 const main = async () => {
   let endDate = new Date();
-  let startDate = add(endDate, { days: -7 });
+  let startDate = add(endDate, { days: -CHUNK_DAY_SIZE });
   let done = false;
   let commits = [];
 
@@ -77,12 +99,13 @@ const main = async () => {
     if (nextCommits.length === 0) {
       done = true;
     }
-    endDate = add(endDate, { days: -7 });
-    startDate = add(startDate, { days: -7 });
+    endDate = add(endDate, { days: -CHUNK_DAY_SIZE });
+    startDate = add(startDate, { days: -CHUNK_DAY_SIZE });
   }
 
   console.log("commits:", commits.length);
 
+  console.log('writing file...')
   fs.writeFileSync(
     `${__dirname}/out/out.json`,
     JSON.stringify(commits, null, 2),
